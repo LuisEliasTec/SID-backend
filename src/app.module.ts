de3a9290bdd384f6cd3c1c8bbcb17f3fbafa8515
7Mica <femicasillasur@ittepic.edu.mx>
2020-12-06 10:30:30 -0700

    Added .env.example file to the project and mongoose support.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        uri: `mongodb://${config.get('DB_SERVER_NAME')}:${config.get(
+          'DB_SERVER_PORT',
+        )}/${config.get('DB_NAME')}`,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
